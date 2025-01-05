960efdc1f7c79c79c16511d8d4828a129c208be4
Aditya Ashok Naik <muddu1403@gmail.com>
2025-01-05 22:17:24 +0530

    Update temperature.py
    Added a few tests.

--- temperature.py
+++ temperature.py
@@ -7,11 +7,58 @@ will have to replace ‘msg’ with a prompt message in quotes), gets input from
 input entered by the user in a string. You will have to write code for displaying the equivalent Fahrenheit
 value on screen.
 Test your function with known inputs to verify if your output is correct or not.
+Nakul Nayak
+12:05 AM
+assert keyword
+if value is incorrect print: "Test 1 failed"
+Nakul Nayak
+12:07 AM
+2. round to 2 decimal places
+
+
 '''
+print("salamalikum")
 
-def celsiusToFahrenheit(temperature):
-    pass
-    # write your code here (and delete the pass statement above)
+def celsiusToFahrenheit(celsius):
+    fahrenheit = (celsius*1.8) + 32
+    assert celsius>-273, "NO VALUE OF TEMPERATURE CAN BE LESS THAN ABSOLUTE 0"
+    assert celsius<100, "MAX. value in celsius scale is 100"
+    return fahrenheit
+
+celsius = float(input("Enter the Degree celsius temperature bossu:"))
+fahrenheit = celsiusToFahrenheit(celsius)
+print("The temperature in the fahrenheit scale is bossu:",round(fahrenheit,2))
 
 
-# write a couple tests for this program. They should call the function celsiusToFahrenheit and check that the returned value is correct
+test1 = celsiusToFahrenheit(37)
+
+test2 = celsiusToFahrenheit(69.6556565)
+
+test3 = celsiusToFahrenheit(101)
+
+test4 = celsiusToFahrenheit(-273)
+
+test5 = celsiusToFahrenheit(-280)
+
+
+#Enter the Degree celsius temperature bossu:37
+#The temperature in th fahrenheit scale is bossu: 98.60000000000001
+#PS C:\Users\Admin\OneDrive\Documents\GitHub\adi-nakul-cs-classroom-c2987d-intro-assignment-lab->
+
+#69
+#Enter the Degree celsius temperature bossu:69
+#The temperature in th fahrenheit scale is bossu: 156.2
+#PS C:\Users\Admin\OneDrive\Documents\GitHub\adi-nakul-cs-classroom-c2987d-intro-assignment-lab->
+
+
+#Enter the Degree celsius temperature bossu:273
+#The temperature in th fahrenheit scale is bossu: 523.4000000000001
+#PS C:\Users\Admin\OneDrive\Documents\GitHub\adi-nakul-cs-classroom-c2987d-intro-assignment-lab-> 
+
+#Enter the Degree celsius temperature bossu:0
+#The temperature in th fahrenheit scale is bossu: 32.0
+#PS C:\Users\Admin\OneDrive\Documents\GitHub\adi-nakul-cs-classroom-c2987d-intro-assignment-lab-
+
+#Enter the Degree celsius temperature bossu:100
+#The temperature in th fahrenheit scale is bossu: 212.0
+#PS C:\Users\Admin\OneDrive\Documents\GitHub\adi-nakul-cs-classroom-c2987d-intro-assignment-lab-
