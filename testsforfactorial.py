e38291948291d53935fc0f17e58c3842fbae0100
Aditya Ashok Naik <muddu1403@gmail.com>
2025-04-19 17:15:33 +0530

    l

--- testsforfactorial.py
+++ testsforfactorial.py
@@ -1,7 +1,7 @@
 from factorial import factorial
 
 testcases = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-expectedresults = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800] 
+expectedresults = [1, 1, 2, 6, 24, 420, 720, 5040, 40320, 362880, 3628800] 
 
 for i in range(len(testcases)):
     assert factorial(testcases[i]) == expectedresults[i]
